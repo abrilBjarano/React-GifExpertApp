94b86896fd15e2bb17724d25f7789de57bc1e0b0
abrilxcx <abrlbjrn@gmail.com>
2024-05-26 02:14:23 -0600

    Updated input from AddCategory.jsx

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 export const AddCategory = () => {
 
+  const [ inputValue, setInputValue ] = useState('');
+
+  const onInputChange = ({ target }) => {
+    setInputValue( target.value );
+  };
+
+  // TODO: Entender bien como funciona la conexión entre todas las funciones y propiedades que están manejando inputValue y setInputValue
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+    console.log( inputValue );
+  };
+
   return (
-    <div>AddCategory</div>
+    <form onSubmit={ onSubmit }>
+      <input
+        type="text"
+        placeholder="Buscar gifs"
+        value={ inputValue }
+        onChange={ onInputChange }
+      />
+    </form>
   )
   
 }
+ 
