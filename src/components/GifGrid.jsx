14eedff6e35de8e98e29186fa0d4bf354c87bf14
abrilxcx <abrlbjrn@gmail.com>
2024-05-27 15:34:13 -0600

    Created getGifs.js helper

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,22 +1,9 @@
+import { getGifs } from "../helpers/getGifs";
+
 export const GifGrid = ({ categoryGifExpertAppProp }) => {
 
-   const getGifs = async() => {
 
-      const url = `https://api.giphy.com/v1/gifs/search?api_key=WZtLUbb5kAarT0hF9HfCavhNLUqHUahK&q=cheeseburgers&limit=8`;
-      const resp = await fetch( url );
-      const { data } = await resp.json();
-
-      const gifs = data.map( ({ id, title, images }) => ({
-         id,
-         title,
-         url: images.downsized_medium.url
-      }));
-      
-
-      console.log(gifs);
-   };
-
-   getGifs();
+   getGifs( categoryGifExpertAppProp );
 
    return (
       <>
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async( category ) => {
+
+   const url = `https://api.giphy.com/v1/gifs/search?api_key=WZtLUbb5kAarT0hF9HfCavhNLUqHUahK&q=${ category }&limit=8`;
+   const resp = await fetch( url );
+   const { data } = await resp.json();
+
+   const gifs = data.map( ({ id, title, images }) => ({
+      id,
+      title,
+      url: images.downsized_medium.url
+   }));
+   
+
+   console.log(gifs);
+};
