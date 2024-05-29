0ab864b1ca9420a302fab9b0936e977a764749f2
abrilxcx <abrlbjrn@gmail.com>
2024-05-28 20:59:21 -0600

    Deleted title <p> from Gifitem.jsx

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -3,7 +3,6 @@ export const GifItem = ({ id, title, url }) => {
    return (
       <div className="card">
          <img src={ url } alt={ title } />
-         <p>{ title }</p>
       </div>
    )
 }
