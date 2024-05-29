6c2889c1f52ff2a04ab36614f3e1c57915cd4ee2
abrilxcx <abrlbjrn@gmail.com>
2024-05-28 20:56:39 -0600

    Updated api_key and limit from getGifs.js

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,8 +1,8 @@
 export const getGifs = async( category ) => {
 
-   const api_key = 'BUZCS4sYWcpdjQdSnVysyy6PomKRW2MO';
+   const api_key = 'Nu9wQ0GY2RzS3mKEvHqAM4IEzt6hlfeU';
 
-   const url = `https://api.giphy.com/v1/gifs/search?api_key=${ api_key }&q=${ category }&limit=5`;
+   const url = `https://api.giphy.com/v1/gifs/search?api_key=${ api_key }&q=${ category }&limit=8`;
    const resp = await fetch( url );
    const { data } = await resp.json();
 
